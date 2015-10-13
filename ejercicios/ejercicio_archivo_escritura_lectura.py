7c0dc067532bf3b817ee0a587f2c92073ad9f76e
lucas <lucaspassa@gmail.com>
2015-10-13 20:17:22 -0300

    Se sube consigna de operaciones con archivos y enlace de referencia para la api

---
+++ ejercicios/ejercicio_archivo_escritura_lectura.py
@@ -0,0 +1,8 @@
+__author__ = 'lucas'
+
+"""
+Generar una clase que sirva para resolver el manejo de lectura y escritura
+y usarla a través de un import.
+
+link a documentación: http://librosweb.es/libro/python/
+"""
--- ejercicios/ejercicio_objetos_calculadora.py
+++ ejercicios/ejercicio_objetos_calculadora.py
@@ -3,11 +3,15 @@
 
 2) agregar a la calculadora multiplicar y dividir.
 
-3) VAlidar que los valores ingresados sean correctos.
+3) Validar que los valores ingresados sean correctos.
 
 Métodos de validación
 http://librosweb.es/libro/python/capitulo_6/metodos_de_validacion.html
 """
+
+class Calculadora:
+
+    def
 
 frase = input("Ingrese una frase: ")
 
